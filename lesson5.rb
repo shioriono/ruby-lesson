4fd09581f44d89127ec466c2c5da4cc2cad38298
EC2 Default User <ec2-user@ip-172-31-46-76.ap-northeast-1.compute.internal>
2023-02-04 04:46:33 +0000

    [Finish]9

--- lesson5.rb
+++ lesson5.rb
@@ -4,6 +4,6 @@ if total_price<100
     puts "みかんを購入。所持金に余りあり。"
 elsif total_price==100
     puts "みかんを購入。所持金は０円です。"
-else total_price>100
+else  total_price>100
     puts "みかんを購入することができません。"
 end
---
+++ lesson9.rb
@@ -0,0 +1,7 @@
+class Car
+  def self.turn(direction)
+    puts "車で#{direction}キロ走ります。"
+  end
+end
+
+Car.turn("右")
